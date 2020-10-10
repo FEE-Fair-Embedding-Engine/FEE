ec97d753ebd06900715b61ec857a95bae9f17e9f
timetraveller_san <anonymous.yolo.wtf@gmail.com>
2020-10-10 00:25:43 +0000

    RTD Compatibility

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Vaibhav Kumar, Tenzin Singhay Bhotia, Vaibhav Kumar'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
